914f0423750d36bf1f5971033bfb299e1aa63fc3
sudh946 <sudh946@gmail.com>
2024-09-15 12:39:06 +0530

    login signup dashboard done

---
+++ main.js
@@ -0,0 +1,21 @@
+
+// loggedin user ke liye hai
+// window.addEventListener('load',() => {
+//     const isloggedIn = localStorage.getItem("loggedInuser");
+
+//     if(isloggedIn){
+//         return
+//     }
+//     if(window.location.pathname === "/dashboard.html"){
+//         window.location="/"
+//     }
+// })
+function signup(){
+    window.location ="login.html"
+}
+function loginup(){
+    window.location ="dashboard.html"
+}
+function questionPage(){
+    window.location ="question.html"
+}
