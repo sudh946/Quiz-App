b27ae9b71997137e2c6dac92d684ec95c3fe004b
sudh946 <sudh946@gmail.com>
2024-09-21 11:07:48 +0530

    updated

--- main.js
+++ main.js
@@ -10,12 +10,39 @@
 //         window.location="/"
 //     }
 // })
-function signup(){
-    window.location ="login.html"
-}
-function loginup(){
+
+function log(){
     window.location ="dashboard.html"
 }
 function questionPage(){
     window.location ="question.html"
+}
+
+function signin(){
+    const fullname = document.getElementById('Full-Name').value;
+    const email = document.getElementById('emailId').value;
+    const password = document.getElementById('Passkey').value;
+
+    const namePattern = /^[a-zA-Z/s/-]+$/
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if(!fullname){
+        alert("Enter your name")
+        return;
+    }
+    if(!namePattern.test(fullname)){
+        alert("Enter the valid Name")
+        return;
+    }
+    if(!emailPattern.test(email)){
+        alert("Enter the valid Email")
+        return;
+    }
+    if(password.length < 8){
+        alert("Enter 8 character password")
+        return;
+    }
+     
+    window.location ="index.html"
+
 }
